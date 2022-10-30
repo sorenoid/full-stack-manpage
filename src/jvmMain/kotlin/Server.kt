640c026ce1e21d58dc8ed110432bcc12af5b424a
sorenoid <sorenoid@gmail.com>
2022-10-29 18:06:00 -0700

    first commit

---
+++ src/jvmMain/kotlin/Server.kt
@@ -0,0 +1,48 @@
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    val manuals = Html2Json().manuals
+    embeddedServer(Netty, 9090) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            allowMethod(HttpMethod.Get)
+            allowMethod(HttpMethod.Post)
+            allowMethod(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+
+        routing {
+            get("/") {
+                call.respondText(
+                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+                    ContentType.Text.Html
+                )
+            }
+            static("/") {
+                resources("")
+            }
+
+            route(Manual.path) {
+                get {
+                    call.respond(manuals.first())
+                }
+            }
+        }
+    }.start(wait = true)
+}
+
